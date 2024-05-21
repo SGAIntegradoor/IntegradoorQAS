8781437368be6a644f53bb042cff0af78c1f7531
SGAIntegrador <developerstrategico2@gmail.com>
2024-05-21 15:39:11 -0500

    QAS With TempUser Invitado hours

--- controladores/controlador.clientes.php
+++ controladores/controlador.clientes.php
@@ -1,19 +1,19 @@
 <?php
-
 require_once "../config/dbconfig.php";
 
 // Obtener parámetros de la solicitud
 $start = $_POST['start'];
 $length = $_POST['length'];
 
-if(isset($_POST['search'])){
-}
-$search = $_POST['search']['value'];
+$search = isset($_POST['search']['value']) ? $_POST['search']['value'] : '';
 
 // Construir la consulta
 $sql = "SELECT * FROM clientes WHERE 1";
 
 if (!empty($search)) {
+    // Convertir la cadena de búsqueda a UTF-8 si es necesario
+    $search = mb_convert_encoding($search, 'UTF-8', 'UTF-8');
+
     $sql .= " AND (cli_nombre LIKE '%$search%' OR cli_email LIKE '%$search%' OR cli_telefono LIKE '%$search%')";
 }
 
@@ -25,6 +25,12 @@ $result = $enlace->query($sql);
 
 $data = array();
 while ($row = $result->fetch_assoc()) {
+    // Convertir los datos a UTF-8 si es necesario
+    foreach ($row as &$value) {
+        $value = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
+    }
+    unset($value); // Limpiar la referencia al último elemento del array
+
     $data[] = $row;
 }
 
@@ -38,6 +44,12 @@ $response = array(
     "data" => $data
 );
 
+// Establecer la cabecera de respuesta como JSON
+header('Content-Type: application/json');
+
+// Imprimir el JSON
 echo json_encode($response);
 
+// Cerrar la conexión a la base de datos
 $enlace->close();
+?>
