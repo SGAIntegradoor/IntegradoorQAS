028eb620946c4657bd72c724b7919b1da710a033
SGAIntegrador <developerstrategico2@gmail.com>
2024-06-28 08:43:00 -0500

    Finesa fixing somethings

--- vistas/js/motos.js
+++ vistas/js/motos.js
@@ -913,10 +913,10 @@ function cotizarFinesaMotos(ofertasCotizaciones) {
               .then((dbResponse) => dbResponse.json())
               .then((dbData) => {
                 const elementDiv = document.getElementById(element.objFinesa);
-                if (dbData.mensaje.includes("Por políticas de Finesa")) {
+                if (dbData.data.mensaje.includes("Por políticas de Finesa")) {
                   cotizacionesFinesaMotos[index].cotizada = true;
                   elementDiv.innerHTML = `Financiación:<br /> No aplica financiación`;
-                } else if(dbData.mensaje.includes("Asegurado no viable para financiacion")) {
+                } else if(dbData.data.mensaje.includes("Asegurado no viable para financiacion")) {
                   cotizacionesFinesaMotos[index].cotizada = true;
                   elementDiv.innerHTML = `Financiación Finesa:<br /> Asegurado no viable para financiación`;
                 } else {
