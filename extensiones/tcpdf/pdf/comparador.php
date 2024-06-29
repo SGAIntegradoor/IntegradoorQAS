48b48aee05bd9236b8d42eec9b327367b457c7cd
SGAIntegrador <developerstrategico2@gmail.com>
2024-06-29 12:30:57 -0500

    Finesa / Max Execution Timeout

--- extensiones/tcpdf/pdf/comparador.php
+++ extensiones/tcpdf/pdf/comparador.php
@@ -2910,4 +2910,3 @@ function productoAseguradora($aseguradora, $producto)
 // Close and output PDF document
 // This method has several options, check the source code documentation for more information.
 $pdf->Output('cotizacionAutos.pdf', 'I');
-mysqli_close($conexion);
