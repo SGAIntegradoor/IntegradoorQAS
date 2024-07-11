96bc7484278ab32c8a74e50d080efeed80eb5049
SGAIntegrador <developerstrategico2@gmail.com>
2024-07-11 14:30:50 -0500

    Cambios cotizar

--- extensiones/tcpdf/pdf/comparadorMotos.php
+++ extensiones/tcpdf/pdf/comparadorMotos.php
@@ -666,7 +666,7 @@ if ($rowValidateF >= 1) {
                 </td>';
 				}
 				$cont3++;
-			} else if ($resultado['Prima'] < "800000") {
+			} else if ($resultado['Prima'] < "800000" && ($resultado['Aseguradora'] != "Liberty" || $resultado['Aseguradora'] != "Seguros Bolivar")) {
 				$html2 .= '<td style="font-size:' . ($font_size - 2) . 'px; color:#666666; font-family:dejavusanscondensedb; text-align: center;" class="puntos td2 ' . $fondo_class . '">
 					No Aplica
 					<br>
