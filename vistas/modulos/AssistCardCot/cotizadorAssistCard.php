9a2efdd854e12b85d978d83b965944c7ab04c67b
SGAIntegrador <developerstrategico2@gmail.com>
2024-08-23 12:42:08 -0500

    Assistcard fixes

--- vistas/modulos/AssistCardCot/cotizadorAssistCard.php
+++ vistas/modulos/AssistCardCot/cotizadorAssistCard.php
@@ -176,7 +176,7 @@
         </div>
         <div class="container-fluid" id="Cards">
             <div class="row">
-                <div class="col-xs-12">
+                <div class="col-xs-12" style="padding-left: 0px;">
                     <p><strong>Nota: </strong>Esta cotización aplica para una sola persona. Si requieres una cotización para un grupo (2 o más personas), ponte en contacto con el equipo comercial a través de tu analista asignado.</p>
                     <p><strong>Nota: </strong>Los valores cotizados se encuentran en dolares americanos (USD).</p>
                 </div>
