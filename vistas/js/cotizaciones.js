fc367b3cb6dae9ea507a198f6e148c465d08fa02
SGAIntegrador <developerstrategico2@gmail.com>
2024-08-30 11:30:43 -0500

    Error Fix: When user dont have connection at the momment when he is going to quotate

--- vistas/js/cotizaciones.js
+++ vistas/js/cotizaciones.js
@@ -1590,7 +1590,7 @@ function editarCotizacion(id) {
 											</div>`;
               } else if (
                 oferta.Manual == "0" &&
-                oferta.Aseguradora == "Previsora" &&
+                oferta.Aseguradora == "Previsora Seguros" &&
                 oferta.UrlPdf !== null &&
                 aseguradoraPermisos == "1"
               ) {
