d248a4047acf075d3f40ae48b891d928fdb882c2
SGAIntegrador <developerstrategico2@gmail.com>
2024-12-23 10:49:03 -0500

    Fixes form edit usuarios

--- controladores/contrasena.controlador.php
+++ controladores/contrasena.controlador.php
@@ -2,8 +2,7 @@
 
 require "../modelos/password.modelo.php";
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
 
   class PasswordController {
 
--- vistas/modulos/miperfil.php
+++ vistas/modulos/miperfil.php
@@ -11,6 +11,9 @@
 //   return;
 // }
 
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+
 
 ?>
 <script>
