c5386ff8029e7cd00ddaad36fdef3fda5517ef61
SGAIntegrador <developerstrategico2@gmail.com>
2024-12-23 23:22:30 -0500

    Fixes form oportunidades

--- vistas/modulos/Oportunidades/views/adminNegociosTable.php
+++ vistas/modulos/Oportunidades/views/adminNegociosTable.php
@@ -582,7 +582,7 @@
         <div class="col-xs-12 col-sm-6 col-md-4 form-group">
           <label for="txtAnalistaGAModal">Analista/asesor GA</label>
           <select type="text" class="form-control" name="txtAnalistaGAModal" id="txtAnalistaGAModal" placeholder="" required>
-            <!-- <option value=""></option> -->
+            <option value=""></option>
           </select>
         </div>
       </div>
