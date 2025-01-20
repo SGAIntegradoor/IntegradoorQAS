a3d893035d3f45ebef0b30c5954e865dd68af631
SGAIntegrador <developerstrategico2@gmail.com>
2025-01-20 07:55:26 -0500

    Updating Oportunidades

--- vistas/modulos/SaludCot/js/eventCotizarSalud.js
+++ vistas/modulos/SaludCot/js/eventCotizarSalud.js
@@ -1276,9 +1276,9 @@ function cotizar() {
           console.log(JSON.stringify(datosCotizacion, null, 2));
 
           $.ajax({
-            
-             url: "https://grupoasistencia.com/health_engine/WSAxa/axa.php",
-             //url: "http://localhost/motorTest/health_engine/axa.php",
+
+            url: "https://grupoasistencia.com/health_engine/WSAxa/axa.php",
+            //url: "http://localhost/motorTest/health_engine/axa.php",
             type: "POST",
             data: JSON.stringify(datosCotizacion),
             success: function (data) {
