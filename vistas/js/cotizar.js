9e972d7da097369426ba68a5956ea29040e4a329
SGAIntegrador <developerstrategico2@gmail.com>
2025-03-20 16:25:06 -0500

    un punto!

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -3392,7 +3392,7 @@ function cotizarOfertas() {
                 let message =
                   aseguradora == "Qualitas"
                     ? `💡 <b>Nueva aseguradora</b> especializada en <b>seguros de autos.</b> La principal aseguradora mexicana de seguros de autos llega a Colombia y <b>nosotros ya tenemos convenio.</b> Solicita cotización manual a tu Analista Comercial.`
-                    : `🔥 <b>Nuevo seguro de autos livianos</b> con modalidad de indemnización arreglo directo para <b>pérdidas parciales</b>.Solicita cotización manual a tu Analista Comercial.`;
+                    : `🔥 <b>Nuevo seguro de autos livianos</b> con modalidad de indemnización arreglo directo para <b>pérdidas parciales</b>. Solicita cotización manual a tu Analista Comercial.`;
 
                 let ofertas = [
                   {
