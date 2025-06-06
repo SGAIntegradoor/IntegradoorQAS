894d62e3115baa80c4ad4fdd5e4896116bbc4516
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-06-06 16:33:40 -0500

    Feat: Change Mod Connection POO

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -3,15 +3,25 @@
 $DB_host = "52.15.158.65";
 $DB_user = "grupoasi_cotizautos";
 $DB_pass = "M1graci0n123";
-$DB_name = "grupoasi_cotizautos_qas";
+$DB_name = "";
 
-try
-{
-	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$URI = explode("/", $_SERVER['REQUEST_URI']);
+
+if (in_array("dev", $URI)) {
+	$DB_name = "grupoasi_cotizautos_dev";
+} elseif (in_array("QAS", $URI) || in_array("qas", $URI)) {
+	$DB_name = "grupoasi_cotizautos_qas";
+} else {
+	$DB_name = "grupoasi_cotizautos";
 }
-catch(PDOException $e)
-{
+
+
+
+try {
+	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}", $DB_user, $DB_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
 	$e->getMessage();
 }
 
