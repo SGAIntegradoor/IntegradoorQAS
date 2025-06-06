a92483bd8ed93cddc8b79a5bc220f1fc3579d80c
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-06-06 15:18:52 -0500

    Feat: Change Mod Connection POO

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,17 +1,27 @@
 <?php
 
+// Documentación del codigo de la conexión a la base de datos
+
+$DB_name = 'grupoasi_cotizautos_dev';
+
+$URI = explode("/", $_SERVER['REQUEST_URI']);
+
+if (in_array("dev", $URI)) {
+	$DB_name = 'grupoasi_cotizautos_dev';
+} elseif (in_array("QAS", $URI) || in_array("qas", $URI)) {
+	$DB_name = 'grupoasi_cotizautos_qas';
+} else {
+	$DB_name = 'grupoasi_cotizautos';
+}
+
 $DB_host = "52.15.158.65";
 $DB_user = "grupoasi_cotizautos";
 $DB_pass = "M1graci0n123";
-$DB_name = "grupoasi_cotizautos_dev";
 
-try
-{
-	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+try {
+	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}", $DB_user, $DB_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
+} catch (PDOException $e) {
 	$e->getMessage();
 }
 
