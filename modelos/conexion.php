64feedae1b6549257ff79b44e5a2d7b79ece28c3
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-08-11 12:19:55 -0500

    QAS to PRD Tested

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -31,7 +31,7 @@ class Conexion{
 
 		if (in_array("dev", $URI)) {
 			self::getInstance()->setEnvironment('grupoasi_cotizautos_dev');
-		} elseif (in_array("QAS", $URI) || in_array("qas", $URI)) {
+		} elseif (in_array("QAS", $URI) || in_array("qas", $URI) || in_array("qas", $URI) || in_array("Pruebas", $URI)) {
 			self::getInstance()->setEnvironment('grupoasi_cotizautos_qas');
 		} else {
 			self::getInstance()->setEnvironment('grupoasi_cotizautos');
