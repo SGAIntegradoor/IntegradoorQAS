fcd432efb7f710e1080d16addaf5958a97aee649
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-09-05 08:07:52 -0500

    L

--- vistas/js/motos.js
+++ vistas/js/motos.js
@@ -2061,7 +2061,7 @@ ${
                   </li>
                   <li class="list-group-item">
                       <span class="badge">* ${GR}</span>
-                      Servicio de Grúa
+                      Asistencia en viajes
                   </li>
               </ul>
           </div>
@@ -2123,7 +2123,7 @@ ${
       <div>VER PDF &nbsp;&nbsp;<span class="fa fa-file-text"></span></div>
   </button>
 </div>`;
-  } else if (aseguradora == "Mundial" && permisosCredenciales == "1" && producto == "Conduce Tranquilo Pes") {
+  } else if (aseguradora == "Mundial" && permisosCredenciales == "1" && producto == "Conduce Tranquilo Mot") {
     cardCotizacion += `
           <div class="col-xs-12 col-sm-6 col-md-2 verpdf-oferta">
               <button id="mundial-pdf${producto}" type="button" class="btn btn-info" onclick='verPdfMundialLivianos(\"${UrlPdf}\")'>
