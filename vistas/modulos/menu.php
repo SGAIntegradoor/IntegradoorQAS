ca8466d6d29d5a1af98bf21e6f6442abaffa0c31
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-09-10 07:49:18 -0500

    Menu Adicional

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -44,8 +44,7 @@ checkUserStatus();
 		let cotHechas = await mostrarCotRestantes();
 		var arrayCot = ["menuCotLiv", "menuCotMot", "menuCotPes", "menuCotMas"];
 		// Verificar si el valor obtenido es válido y está definido
-		if (permisosCotizacionesTotales !== null && permisosCotizacionesTotales !== undefined) {
-		}
+		if (permisosCotizacionesTotales !== null && permisosCotizacionesTotales !== undefined) {}
 	});
 </script>
 <style>
@@ -103,8 +102,18 @@ checkUserStatus();
 				</a>
 			</li>
 
+			
+
 
 			<?php
+			if (in_array($_SESSION['rol'], [1, 10, 11, 12, 22])) {
+				echo '<li id="menuCRM">
+			<a href="http://localhost:5173/" target="_blank">
+			<i class="fas fa-users"></i>
+			<span>CRM</span>
+			</a>
+			</li>';
+			}
 			/*=============================================
 		CONFIGURAR POLÍTICIAS
 		=============================================*/
@@ -235,6 +244,12 @@ checkUserStatus();
 			</li>';
 			}
 
+			
+
+			// <li class="' . ($currentPage == "productividad" ? "active bg-li-active" : "hoverLi") . '" style="border: 1px solid gray; border-radius: 5px; padding: 5px;">
+			// 	<a href="productividad">Productividad</a>
+			// </li>
+
 			/*=============================================
 		NEGOCIOS
 		=============================================*/
@@ -253,11 +268,11 @@ checkUserStatus();
 						</li>
 						</ul>
 						</li>';
-					}
-					
-					// <li class="' . ($currentPage == "productividad" ? "active bg-li-active" : "hoverLi") . '" style="border: 1px solid gray; border-radius: 5px; padding: 5px;">
-					// 	<a href="productividad">Productividad</a>
-					// </li>
+			}
+
+			// <li class="' . ($currentPage == "productividad" ? "active bg-li-active" : "hoverLi") . '" style="border: 1px solid gray; border-radius: 5px; padding: 5px;">
+			// 	<a href="productividad">Productividad</a>
+			// </li>
 
 			/*=============================================
 		HOGAR
