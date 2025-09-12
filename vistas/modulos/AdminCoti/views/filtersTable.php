a0b3bf7ce59b2cf9170994b1b596a19bab0c1089
javier <javiergaviria22@gmail.com>
2025-09-12 09:46:01 -0500

    fix(admin-cotizaciones): ajustar mapeo de clase Camioneta Repartidora y filtro de livianos utilitarios

--- vistas/modulos/AdminCoti/views/filtersTable.php
+++ vistas/modulos/AdminCoti/views/filtersTable.php
@@ -29,7 +29,8 @@
                         <label for="moduloCotizacion">Módulo de cotización:</label>
                         <select type="text" class="form-control" name="moduloCotizacion" id="moduloCotizacion" placeholder="Modulo de cotización">
                             <option value=""></option>
-                            <option value="1">Livianos</option>
+                            <option value="1">Livianos Familiares</option>
+                            <option value="5">Livianos Utilitarios</option>
                             <option value="2">Pesados</option>
                             <option value="3">Motos</option>
                             <option value="4">Transporte Pasajeros</option>
