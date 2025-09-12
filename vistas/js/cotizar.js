11eb08e425cdf6a8cfb9ab1dfa0df26c9438d70d
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-09-12 16:42:27 -0500

    Fixed some things on cotizar

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -4513,7 +4513,7 @@ function cotizarOfertas() {
           let body = JSON.parse(requestOptions.body);
           body.plan = aseguradora;
           requestOptions.body = JSON.stringify(body);
-          const libertyPromise = comprobarFallida(aseguradora)
+          const libertyPromise = comprobarFallida("HDI Seguros")
           // const libertyPromise = comprobarFallida("HDI Seguros")
             ? fetch(
                 "https://grupoasistencia.com/motor_webservice/Liberty_autos",
