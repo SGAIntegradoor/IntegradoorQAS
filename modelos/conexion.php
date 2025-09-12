8ca86d38f2bc6af70edf13a4d0d0c7f7453722f2
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-09-11 23:28:22 -0500

    Connection updated on modulos

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -3,22 +3,28 @@
 class Conexion{
 
 	public $dbname = 'grupoasi_cotizautos_dev';
+	public $host = 'localhost:3307';
 
-	public function __construct($dbname = null){
+	public function __construct($dbname = null, $host = null){
 		if($dbname != null){
 			$this->dbname = $dbname;
 		}
+		if($host != null){
+			$this->host = $host;
+		}
 	}
 
-	public function setEnvironment($dbname){
+	public function setEnvironment($dbname, $host){
 		$this->dbname = $dbname;
+		$this->host = $host;
 	}
 
-	public static function getInstance($dbname = null){
+	public static function getInstance($dbname = null, $host = null){
 		static $instance = null;
 
 		if ($instance === null) {
 			$instance = new self($dbname);
+			$instance->host = $host;
 		}
 
 		return $instance;
@@ -30,14 +36,14 @@ class Conexion{
 		$URI = explode("/", $_SERVER['REQUEST_URI']);
 
 		if (in_array("dev", $URI)) {
-			self::getInstance()->setEnvironment('grupoasi_cotizautos_dev');
-		} elseif (in_array("QAS", $URI) || in_array("qas", $URI) || in_array("Pruebas", $URI) || in_array("pruebas", $URI)) {
-			self::getInstance()->setEnvironment('grupoasi_cotizautos_qas');
+			self::getInstance()->setEnvironment('grupoasi_cotizautos_dev', 'localhost:3307');
+		} elseif (in_array("QAS", $URI) || in_array("qas", $URI) || in_array("qas", $URI) || in_array("Pruebas", $URI)) {
+			self::getInstance()->setEnvironment('grupoasi_cotizautos_qas', "localhost");
 		} else {
-			self::getInstance()->setEnvironment('grupoasi_cotizautos');
+			self::getInstance()->setEnvironment('grupoasi_cotizautos', "localhost");
 		}
 
-		$link = new PDO("mysql:host=52.15.158.65;dbname=". self::getInstance()->dbname,
+		$link = new PDO("mysql:host=". self::getInstance()->host .";dbname=". self::getInstance()->dbname,
 			            "grupoasi_cotizautos",
 			            'M1graci0n123');
 		$link->exec("set names utf8");
