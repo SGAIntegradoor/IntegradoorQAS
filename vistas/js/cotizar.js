9c9448ad7bc16948452ef12a5e88302019a5918b
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-10-15 01:39:20 -0500

    Removed Env on Body

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -5364,7 +5364,7 @@ function cotizarOfertas() {
           cre_sol_token: cre_sol_token,
           cre_sol_fecha_token: cre_sol_fecha_token,
         },
-        env: "QAS",
+        //env: "QAS",
       };
 
       //! Agregar a Motos y Pesados START
