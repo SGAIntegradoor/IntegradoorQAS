51fa4c32d1533501cfc19c648c9c500a4a3e723a
Javier Jose Gaviria <javiergaviria22@gmail.com>
2025-10-20 16:53:40 -0500

    Minor Fixes

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -275,8 +275,8 @@ checkUserStatus();
 		HOGAR
 		=============================================*/
 			if ($_SESSION["intermediario"] == "3" || $_SESSION["intermediario"] == "149" || $_SESSION["idUsuario"] == 34) {
-				echo '<li id="menuCotHog" class="' . ($currentPage == 'hogar' ? 'active' : '') . '">
-			<a href="hogar">
+				echo '<li id="menuCotHog" class="' . ($currentPage == 'hogar2' ? 'active' : '') . '">
+			<a href="hogar2">
 			<i class="fa-solid fa-house-circle-check"></i>
 			<span>Cotizar Hogar</span>
 			</a>
