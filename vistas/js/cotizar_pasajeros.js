fc61673d464489aca49afe0f240d5da009ead448
Daniel Ospina Ramirez <danielospinar@gmail.com>
2025-10-21 16:17:02 -0500

    Taxis Mundial

--- vistas/js/cotizar_pasajeros.js
+++ vistas/js/cotizar_pasajeros.js
@@ -2416,7 +2416,7 @@ const mostrarOferta = (
   } else if (
     aseguradora == "Mundial" &&
     permisosCredenciales == "1" &&
-    (producto == "Seguro Amarillo" || producto == "Seguro Amarillo RC en Exceso")
+    (producto == "Seguro Amarillo" || producto == "Seguro Amarillo - RC en Exceso")
   ) {
     cardCotizacion += `
           <div class="col-xs-12 col-sm-6 col-md-2 verpdf-oferta">
